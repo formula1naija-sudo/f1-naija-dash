4480fc26f07fbb575837b96a66279b9c276fdb08
formula1naija-sudo <formula1naija@gmail.com>
2026-03-06 22:32:02 +0000

    Update useSidebarStore.ts

--- dashboard/src/stores/useSidebarStore.ts
+++ dashboard/src/stores/useSidebarStore.ts
@@ -3,25 +3,21 @@ import { create } from "zustand";
 type SidebarStore = {
 	pinned: boolean;
 	opened: boolean;
-
 	close: () => void;
 	open: () => void;
-
 	pin: () => void;
 	unpin: () => void;
 };
 
 export const useSidebarStore = create<SidebarStore>()((set) => ({
-	pinned: true,
+	pinned: false,
 	opened: false,
-
 	close: () => {
 		set({ opened: false });
 	},
 	open: () => {
 		set({ opened: true });
 	},
-
 	pin: () => {
 		set({ pinned: true, opened: false });
 	},
