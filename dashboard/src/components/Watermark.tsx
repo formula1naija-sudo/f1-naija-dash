fd28337ad5690480ba5b8ff00a131c09d690b83b
formula1naija-sudo <formula1naija@gmail.com>
2026-03-08 10:28:04 +0000

    fix: replace img with Next.js Image in Watermark to fix no-img-element lint

--- dashboard/src/components/Watermark.tsx
+++ dashboard/src/components/Watermark.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Watermark() {
   return (
     <div className="pointer-events-none fixed bottom-4 left-4 z-50 opacity-40">
-      <img src="/tag-logo.png" alt="F1 Naija" width={48} height={48} className="rounded-full" />
+      <Image src="/tag-logo.png" alt="F1 Naija" width={48} height={48} className="rounded-full" />
     </div>
   );
 }
