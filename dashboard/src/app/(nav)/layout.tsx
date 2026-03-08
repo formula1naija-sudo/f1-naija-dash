bb3dcde278938b0902d47d8c8fd82f9ff9f33f11
formula1naija-sudo <formula1naija@gmail.com>
2026-03-08 01:07:26 +0000

    Update layout.tsx

--- dashboard/src/app/(nav)/layout.tsx
+++ dashboard/src/app/(nav)/layout.tsx
@@ -1,67 +1,58 @@
 import { type ReactNode } from "react";
-import Image from "next/image";
 import Link from "next/link";
 
 import Footer from "@/components/Footer";
+import AdBanner from "@/components/AdBanner";
 
 type Props = {
-    children: ReactNode;
+	children: ReactNode;
 };
 
 export default function Layout({ children }: Props) {
-    return (
-        <>
-            <nav className="sticky top-0 left-0 z-10 flex h-12 w-full items-center justify-between gap-4 border-b border-zinc-800 p-2 px-4 backdrop-blur-lg">
-                <div className="flex gap-4">
-                    <Link className="transition duration-100 active:scale-95" href="/">
-                        Home
-                    </Link>
-                    <Link className="transition duration-100 active:scale-95" href="/dashboard">
-                        Dashboard
-                    </Link>
-                    <Link className="transition duration-100 active:scale-95" href="/schedule">
-                        Schedule
-                    </Link>
-                    <Link className="transition duration-100 active:scale-95" href="/news">
-                        News
-                    </Link>
-                    <Link className="transition duration-100 active:scale-95" href="/help">
-                        Help
-                    </Link>
-                </div>
+	return (
+		<>
+			<AdBanner />
 
-                <div className="hidden items-center gap-4 pr-2 sm:flex">
-                    <Link
-                        className="flex items-center gap-2 transition duration-100 active:scale-95"
-                        href="https://x.com/f1_naija"
-                        target="_blank"
-                    >
-                        <span>X / Twitter</span>
-                    </Link>
+			<nav className="sticky top-10 left-0 z-10 flex h-12 w-full items-center justify-between gap-4 border-b border-zinc-800 p-2 px-4 backdrop-blur-lg">
+				<div className="flex gap-4">
+					<Link className="transition duration-100 active:scale-95" href="/">
+						Home
+					</Link>
+					<Link className="transition duration-100 active:scale-95" href="/dashboard">
+						Dashboard
+					</Link>
+					<Link className="transition duration-100 active:scale-95" href="/schedule">
+						Schedule
+					</Link>
+					<Link className="transition duration-100 active:scale-95" href="/help">
+						Help
+					</Link>
+				</div>
 
-                    <Link
-                        className="flex items-center gap-2 transition duration-100 active:scale-95"
-                        href="https://www.instagram.com/f1_naija/"
-                        target="_blank"
-                    >
-                        <span>Instagram</span>
-                    </Link>
+				<div className="hidden items-center gap-4 pr-2 sm:flex">
+					<Link
+						className="flex items-center gap-2 transition duration-100 active:scale-95"
+						href="https://x.com/f1naija"
+						target="_blank"
+					>
+						<span>X / Twitter</span>
+					</Link>
 
-                    <Link
-                        className="flex items-center gap-2 transition duration-100 active:scale-95"
-                        href="https://www.tiktok.com/@f1.naija"
-                        target="_blank"
-                    >
-                        <span>TikTok</span>
-                    </Link>
-                </div>
-            </nav>
+					<Link
+						className="flex items-center gap-2 transition duration-100 active:scale-95"
+						href="https://instagram.com/f1naija"
+						target="_blank"
+					>
+						<span>Instagram</span>
+					</Link>
+				</div>
+			</nav>
 
-            <main className="container mx-auto max-w-(--breakpoint-lg) px-4">
-                {children}
+			<main className="container mx-auto max-w-(--breakpoint-lg) px-4">
+				{children}
 
-                <Footer />
-            </main>
-        </>
-    );
+				<Footer />
+			</main>
+		</>
+	);
 }
